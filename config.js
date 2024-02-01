6ba7bd1ce44721f1b8f76edf60bc02c19b50a035
OctoLinkYT <138608589+OctoLinkYT@users.noreply.github.com>
2024-02-01 16:41:10 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 window.__CONFIG__ = {
   // The URL for the CORS proxy, the URL must NOT end with a slash!
-  VITE_CORS_PROXY_URL: "CHANGEME",
+  VITE_CORS_PROXY_URL: "https://gilded-vacherin-31376c.netlify.app",
 
   // The READ API key to access TMDB
-  VITE_TMDB_READ_API_KEY: "CHANGEME",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYzM0NDBmMjdiMWFmZmNlNGUzZGU1MWQ0MjgxZGFmMCIsInN1YiI6IjY1YmMwZjI2MWZkMzZmMDE3ZDczNDFmZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.GA0uYYBZQkjukqKMbqCKyMcw4YHD8e2iRAsuKXN06X4",
 
   // The DMCA email displayed in the footer, null to hide the DMCA link
   VITE_DMCA_EMAIL: null,
